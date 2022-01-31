238a82ad84d98f94280a46d0a9e1afcc742c4304
Jacqueline Harnish <jacquelineharnish@jacquelines-mbp.myfiosgateway.com>
2022-01-31 16:22:34 -0500

    move settings to main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,16 @@ import {
   TextField,
   Button,
   Typography,
-  IconButton,
-  Box,
-  Modal,
   FormGroup,
   FormControl,
   FormControlLabel,
   FormLabel,
   Switch,
 } from '@mui/material';
-import SettingsIcon from '@mui/icons-material/Settings';
-import CloseIcon from '@mui/icons-material/Close';
-
-const settingsStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 220,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: 4,
-};
 
 function App() {
   const [len, setLen] = useState(14);
   const [password, setPassword] = useState('p@ssw0rd');
-  const [openSettings, setOpenSettings] = useState(false);
   const [needsUpper, setNeedsUpper] = useState(true);
   const [needsNums, setNeedsNums] = useState(true);
   const [needsSpecial, setNeedsSpecial] = useState(true);
@@ -81,14 +64,6 @@ function App() {
     setLen(parseInt(event.target.value, 10));
   };
 
-  const handleOpenSettings = () => {
-    setOpenSettings(true);
-  };
-
-  const handleCloseSettings = () => {
-    setOpenSettings(false);
-  };
-
   const handleChangeUpper = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNeedsUpper(event.target.checked);
   };
@@ -102,69 +77,54 @@ function App() {
   };
 
   return (
-    <>
-      <IconButton color="primary" aria-label="settings" style={{ position: 'absolute', top: '10px', right: '10px' }} size="large" onClick={handleOpenSettings}>
-        <SettingsIcon fontSize="inherit" />
-      </IconButton>
-      <Modal
-        open={openSettings}
-        onClose={handleCloseSettings}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={settingsStyle}>
-          <IconButton style={{ position: 'absolute', top: '5px', right: '5px' }} onClick={handleCloseSettings}>
-            <CloseIcon />
-          </IconButton>
-          <FormControl component="fieldset" variant="standard">
-            <FormLabel component="legend">Password constraints</FormLabel>
-            <FormGroup>
-              <FormControlLabel control={<Switch onChange={handleChangeUpper} checked={needsUpper} />} label="Uppercase letters" />
-              <FormControlLabel control={<Switch onChange={handleChangeNums} checked={needsNums} />} label="Numbers" />
-              <FormControlLabel control={<Switch onChange={handleChangeSpecial} checked={needsSpecial} />} label="Special chars" />
-              <TextField
-                id="outlined-number"
-                label="Length"
-                type="number"
-                variant="standard"
-                InputProps={{ inputProps: { min: 8, max: 100 } }}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                value={len}
-                onChange={handleLenChange}
-                style={{ marginTop: '5px' }}
-              />
-            </FormGroup>
-          </FormControl>
-        </Box>
-      </Modal>
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: '100vh' }}
-      >
-        <Grid item xs={3} style={{ maxWidth: '400px', padding: '0 10px' }}>
-          <Grid
-            container
-            spacing={2}
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid item>
-              <Typography variant="h4" style={{ wordBreak: 'break-all', marginBottom: '10px' }}>{password}</Typography>
-            </Grid>
-            <Grid item>
-              <Button variant="contained" onClick={handleSubmit}>Generate</Button>
-            </Grid>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <Grid item xs={3} style={{ maxWidth: '400px', padding: '0 10px' }}>
+        <Grid
+          container
+          spacing={4}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item>
+            <Typography variant="h4" style={{ wordBreak: 'break-all' }}>{password}</Typography>
+          </Grid>
+          <Grid item>
+            <FormControl component="fieldset" variant="standard">
+              <FormLabel component="legend">Password constraints</FormLabel>
+              <FormGroup>
+                <FormControlLabel control={<Switch onChange={handleChangeUpper} checked={needsUpper} />} label="Uppercase letters" />
+                <FormControlLabel control={<Switch onChange={handleChangeNums} checked={needsNums} />} label="Numbers" />
+                <FormControlLabel control={<Switch onChange={handleChangeSpecial} checked={needsSpecial} />} label="Special chars" />
+                <TextField
+                  id="outlined-number"
+                  label="Length"
+                  type="number"
+                  variant="standard"
+                  InputProps={{ inputProps: { min: 8, max: 100 } }}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  value={len}
+                  onChange={handleLenChange}
+                  style={{ marginTop: '5px' }}
+                />
+              </FormGroup>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={handleSubmit}>Generate</Button>
           </Grid>
         </Grid>
       </Grid>
-    </>
+    </Grid>
   );
 }
 
